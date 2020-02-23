226678ba644626c7022afc9685569ec6ce669412
scalerone <676119236@qq.com>
2020-02-23 23:07:51 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    base: '/blog-demo/',
+    title: '小辉同学的技术博客',
+    description: 'Vuepress blog demo',
+    themeConfig: {
+      // 你的GitHub仓库，请正确填写
+      repo: 'https://github.com/scalerone/blog',
+      // 自定义仓库链接文字。
+      repoLabel: 'My GitHub',
+      nav: [
+          { text: 'Home', link: '/' },
+          { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+      ],
+      sidebar: [
+        ['/', '首页'],
+        ['/blog/FirstBlog.md', '我的第一篇博客'],
+        {
+          title: 'HTML',
+          collapsable: true,
+          children: [
+            ['page1/新增标签','新增标签']
+         
+          ]
+        }
+      ]
+  }
+
+  }
